d085a64f22d530c67679d58984818a535d79d0d7
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-07-24 17:46:52 +0100

    genre album page

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -6,8 +6,23 @@ export default async function AlbumPage({ params }) {
 	const { id } = await params;
 
 	const info = (
-		await db.query(`SELECT * FROM albums WHERE albums.id = $1`, [id])
+		await db.query(
+			`SELECT albums.*, ARRAY_AGG(genres.type) AS genre
+			FROM albums
+
+			INNER JOIN albums_genres
+			ON albums.id = albums_genres.album_id
+
+			INNER JOIN genres
+			ON albums_genres.genre_id = genres.id
+
+			WHERE albums.id = $1
+			GROUP BY albums.id`,
+			[id]
+		)
 	).rows;
+
+	console.log(info);
 
 	return (
 		<main className="grid grid-cols-8 gap-4 py-24">
@@ -27,9 +42,22 @@ export default async function AlbumPage({ params }) {
 							<h2>{album.artist}</h2>
 							<h1>{album.title}</h1>
 						</div>
-						<div>
+
+						<div className="flex flex-col gap-[1rem]">
 							<p>Released: {album.year}</p>
-							<p>Genre: {album.genre}</p>
+
+							<div className="flex flex-wrap gap-[1rem] items-center">
+								<p>Genre: </p>
+
+								{album.genre.map((g) => (
+									<p
+										key={g}
+										className="bg-blue-50 text-black w-fit px-[1.5rem] py-[0.25rem] rounded-full"
+									>
+										{g}
+									</p>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
