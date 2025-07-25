20ddc4ca2945c62848fdd90e15ecfb5553332fb7
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-07-25 14:34:03 +0100

    clerk2

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,12 +35,14 @@ export default function RootLayout({ children }) {
 					<header className="flex justify-end items-center p-4 gap-4 h-16">
 						<SignedOut>
 							<SignInButton />
+
 							<SignUpButton>
 								<button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
 									Sign Up
 								</button>
 							</SignUpButton>
 						</SignedOut>
+
 						<SignedIn>
 							<UserButton />
 						</SignedIn>
--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,30 @@
-import {
-	clerkMiddleware,
-	auth,
-	createRouteMatcher,
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([`/profile(.*)`]);
 
+const isOnboardingRoute = createRouteMatcher([`/onboarding`]);
+
 export default clerkMiddleware(async (auth, req) => {
-	if (isProtectedRoute(req)) {
-		await auth.protect();
+	const { userId, sessionClaims, redirectToSignIn } = await auth();
+
+	// if the user is not signed in and route is protected, redirect to sign-in
+	if (!userId && isProtectedRoute(req)) {
+		return redirectToSignIn({ returnBackUrl: req.url });
 	}
+
+	// catch and redirect users who do not have onboarding completed
+	if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+		return new URL(`/onboarding`, req.url);
+	}
+
+	// if user is signed in and on a protected route, let them access it
+	if (userId && isProtectedRoute(req)) {
+		return auth.next();
+	}
+
+	// if (isProtectedRoute(req)) {
+	// 	await auth.protect();
+	// }
 });
 
 export const config = {
