073be58153c123928fc27c9034154852e96dd8a0
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-07-25 11:55:16 +0100

    clerk1

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,15 @@ const geistMono = Geist_Mono({
 	subsets: ['latin'],
 });
 
+import {
+	ClerkProvider,
+	SignInButton,
+	SignUpButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from '@clerk/nextjs';
+
 export const metadata = {
 	title: 'Music Catalog',
 	description: 'A next app',
@@ -18,12 +27,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<header className="flex justify-end items-center p-4 gap-4 h-16">
+						<SignedOut>
+							<SignInButton />
+							<SignUpButton>
+								<button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+									Sign Up
+								</button>
+							</SignUpButton>
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</header>
+
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
